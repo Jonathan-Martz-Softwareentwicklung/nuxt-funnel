6f17f0db6e9bf1f94219137c40019a068bcb2ef5
Jonathan Martz <support@habit-streak.app>
2025-05-01 00:45:51 +0200

    bun update, update content

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="page bg-white px-3 py-3">
     <a href="https://www.jonathan-martz.de?ref=anfragen" target="_blank" rel="noopener noreferrer">
-      <img src="https://cdn.jmse.cloud/jmse-logo.svg" class="mx-auto" alt="">
+      <img src="https://cdn.jmse.cloud/jmse-logo.svg" />
     </a>
     <section class="grid grid-cols-6 gap-3 prose">
       <div class="col-span-6 md:col-span-2">
-        <img src="https://cdn.jmse.cloud/avatar-2024-modified.png" alt="">
+        <img src="https://cdn.jmse.cloud/avatar-2024-modified.png" />
       </div>
       <div class="col-span-6 md:col-span-4">
         <h3 class="font-bold text-lg text-black">Wer bin ich ?</h3>
@@ -33,7 +33,7 @@
       </div>
       <div class="col-span-6">
         <div class="divider divider-primary text-black mt-0">Anfragen</div>
-        <form action="" class="grid grid-cols-6 gap-3" data-theme="light">
+        <form v-if="route.query.done != 'success'" @submit.prevent="send" class="grid grid-cols-6 gap-3" data-theme="light">
           <div class="col-span-6 md:col-span-2">
             <div class="form-control">
               <label class="floating-label">
@@ -54,7 +54,7 @@
             <div class="form-control">
               <label class="floating-label">
                 <span>Telefon</span>
-                <input type="email" v-model="email" placeholder="02261 58639" class="input w-full" />
+                <input type="text" v-model="telefon" placeholder="02261 58639" class="input w-full" />
               </label>
             </div>
           </div>
@@ -73,7 +73,7 @@
           <div class="col-span-6 md:col-span-3">
             <label class="floating-label">
               <span>Was ist deine Preis Vorstellung ?</span>
-              <select name="" id="" class="select w-full">
+              <select v-model="price" class="select w-full">
                 <option value="200-500">200-500 €</option>
                 <option value="500-2500">500-2500 €</option>
               </select>
@@ -83,15 +83,44 @@
             <button type="submit" class="btn btn-sm btn-primary btn-block px-3 py-3">Formular abschicken</button>
           </div>
         </form>
+        <div v-else>
+          <div class="alert alert-success block text-center text-white">
+            Anfrage erfolgreich verschickt
+          </div>
+        </div>
       </div>
     </section>
   </section>
 </template>
 
 <script lang="ts" setup>
+import PocketBase from 'pocketbase'
+import {useRoute,useRouter} from 'vue-router'
+
 const name = ref('');
 const email = ref('');
+const telefon = ref('');
 const typ = ref('webseite');
+const price = ref('200-500');
+
+const route = useRoute();
+const router = useRouter();
+
+const send = async () => {
+  const pb = new PocketBase('https://admin.funnel.jmse.cloud');
+
+  let response = await pb.collection('anfragen').create({
+    name: name.value,
+    telefon: telefon.value,
+    email: email.value,
+    typ: typ.value,
+    price: price.value,
+  });
+
+  if(response.id){
+    router.push('/done=success');
+  }
+}
 </script>
 
 <style>
