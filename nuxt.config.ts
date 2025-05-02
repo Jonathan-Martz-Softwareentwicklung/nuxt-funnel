3b2e6f6668d33508a583e09932b9244667d57a7d
Jonathan Martz <support@habit-streak.app>
2025-05-02 23:16:31 +0200

    install plausbile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,18 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ['~/tailwind.css'],
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+  plausible: {
+    // Prevent tracking on localhost
+    ignoredHostnames: ['localhost'],
+    domain: 'funnel.jmse.cloud',
+    apiHost: 'https://tracking.jmse.cloud'
+  },
+
+  modules: ["@nuxtjs/plausible"],
 });
